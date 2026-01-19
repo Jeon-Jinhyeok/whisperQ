f61265ea2eec87b379b666494486a30f994c9123
jinhyeok <aqwstn@gmail.com>
2026-01-19 21:53:36 +0900

    Init Project

---
+++ src/main/java/com/example/whisperQ/WhisperQApplication.java
@@ -0,0 +1,13 @@
+package com.example.whisperQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhisperQApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhisperQApplication.class, args);
+	}
+
+}
